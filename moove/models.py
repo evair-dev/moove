1cdc7adbfb8c8f597c87c457b4e2e2f0aee08f31
Evair Macollunco <evair-dev@Evairs-MacBook-Pro.local>
2021-12-27 00:23:47 -0500

    Create model user and schemas
    Model user was created and simple schemas were added

---
+++ moove/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True, index=True)
+    full_name = Column(String(150), nullable=False)
+    photo_path = Column(String)
+    password_hash = Column(String(100), nullable=False)
+    # Add password_salt when we authenticate
+    # password_salt = Column(String)  Add in
+
+
